39d2c74eec22471716c9f8097645a4366ab1b465
Henry Calderon <theceintiant@gmail.com>
2021-05-14 17:17:32 -0700

    Access-Control-Allow-Origin thing added

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ app.use(cookieSession({
 app.use(passport.initialize()); // Used to initialize passport
 app.use(passport.session()); // Used to persist login sessions
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 var userProfile;
 
 // Strategy config
